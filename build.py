c91f3085eb5c6583764fd43425c63c10ac286531
Marco Hoyer <marco@hoyer.pm>
2014-08-28 01:59:14 +0200

    Initial commit

---
+++ build.py
@@ -0,0 +1,19 @@
+
+from pybuilder.core import use_plugin, init, Author
+
+use_plugin("python.install_dependencies")
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.distutils")
+use_plugin('copy_resources')
+use_plugin("python.coverage")
+
+name = "boto-happytrail"
+default_task = "publish"
+
+@init
+def initialize(project):
+
+    project.build_depends_on("mock")
+    project.depends_on("boto")
+
